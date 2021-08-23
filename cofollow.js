cd24dd8a78a552baaad35e44171c2b887525100d
Pavel Husakouski <phusakouski@gmail.com>
2021-08-23 15:28:46 +0300

    cofollow

--- cofollow.js
+++ cofollow.js
@@ -1,13 +1,37 @@
-function start(generator) {
-    generator.next();
+// cofollow.py
+//
+// A simple example showing how to hook up a pipeline with
+// coroutines.   To run this, you will need a log file.
+// Run the program logsim.py in the background to get a data
+// source.
 
-    return generator;
+const {coroutine} = require('./coroutine');
+const {TextReader} = require('./text-reader');
+const time = require('./time');
+
+function follow(reader, target) {
+    while (true) {
+        let line = reader.readline();
+
+        if (!line) {
+            time.sleep(100);    // Sleep briefly
+            continue;
+        }
+        target.next(line);
+    }
 }
 
-function* printer() {
-    while(true) {
+const printer = coroutine(function* () {
+    while (true) {
         let line = yield;
 
         console.log(line);
     }
+});
+
+if (require.main === module) {
+    const filename = 'access-log';
+    const reader = TextReader.open(filename, TextReader.filesize(filename));
+
+    follow(reader, printer());
 }
